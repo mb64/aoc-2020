a3c785af32d349acf94e40c654c2b6a425663fa1
Mark Barbone <mark.l.barbone@gmail.com>
2020-12-04 19:19:48 -0800

    more day 1 benches

--- 01/a/benches/my_benchmark.rs
+++ 01/a/benches/my_benchmark.rs
@@ -1,4 +1,4 @@
-use a::{hm, somebody};
+use a::*;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 fn input() -> Vec<i32> {
@@ -11,8 +11,14 @@ fn input() -> Vec<i32> {
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     let vals = input();
-    c.bench_function("hash table", |b| b.iter(|| hm(black_box(&vals[..]))));
-    c.bench_function("somebody", |b| b.iter(|| somebody(black_box(&vals[..]))));
+    c.bench_function("O(n³)", |b| b.iter(|| lin_2(black_box(&vals[..]))));
+    c.bench_function("O(n³) early continue", |b| {
+        b.iter(|| lin(black_box(&vals[..])))
+    });
+    c.bench_function("O(n²)", |b| b.iter(|| hm_2(black_box(&vals[..]))));
+    c.bench_function("O(n²) early continue", |b| {
+        b.iter(|| hm(black_box(&vals[..])))
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
--- 01/a/src/lib.rs
+++ 01/a/src/lib.rs
@@ -1,26 +1,6 @@
 use std::collections::HashSet;
-use std::time::Instant;
 
-// fn main() {
-//     let vals: Vec<i32> = include_str!("input.txt")
-//         .lines()
-//         .map(|l| l.parse())
-//         .collect::<Result<_, _>>()
-//         .unwrap();
-
-//     let now = Instant::now();
-//     println!(
-//         "result: {}",
-//         (0..100).map(|_| somebody(&vals[..])).sum::<i64>()
-//     );
-//     println!("{:?} (somebody)", now.elapsed());
-
-//     let now = Instant::now();
-//     println!("result: {}", (0..100).map(|_| hm(&vals[..])).sum::<i64>());
-//     println!("{:?} (hm)", now.elapsed());
-// }
-
-pub fn somebody(vals: &[i32]) -> i64 {
+pub fn lin(vals: &[i32]) -> i64 {
     for i in 0..vals.len() - 2 {
         let x = vals[i];
         for j in i + 1..vals.len() - 1 {
@@ -28,6 +8,19 @@ pub fn somebody(vals: &[i32]) -> i64 {
             if x + y >= 2020 {
                 continue;
             }
+            if vals[j + 1..].contains(&(2020 - x - y)) {
+                return (x as i64) * (y as i64) * ((2020 - x - y) as i64);
+            }
+        }
+    }
+    panic!()
+}
+
+pub fn lin_2(vals: &[i32]) -> i64 {
+    for i in 0..vals.len() - 2 {
+        let x = vals[i];
+        for j in i + 1..vals.len() - 1 {
+            let y = vals[j];
             if vals[j + 1..].contains(&(2020 - x - y)) {
                 return (x as i64) * (y as i64) * ((2020 - x - y) as i64);
             }
@@ -46,6 +39,21 @@ pub fn hm(vals: &[i32]) -> i64 {
             if x + y >= 2020 {
                 continue;
             }
+            if hs.contains(&(2020 - x - y)) {
+                return (x as i64) * (y as i64) * ((2020 - x - y) as i64);
+            }
+        }
+    }
+    panic!()
+}
+
+pub fn hm_2(vals: &[i32]) -> i64 {
+    let hs: HashSet<i32> = vals.iter().copied().collect();
+
+    for i in 0..vals.len() - 2 {
+        let x = vals[i];
+        for j in i + 1..vals.len() - 1 {
+            let y = vals[j];
             if hs.contains(&(2020 - x - y)) {
                 return (x as i64) * (y as i64) * ((2020 - x - y) as i64);
             }
